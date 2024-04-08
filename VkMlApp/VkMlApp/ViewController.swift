55dbf0c98f48c5e75a86b98920bae9b3b439a0ac
Mikhail Ulanov <mixaulanoff@yandex.ru>
2024-04-08 12:44:15 +0300

    create a project and delete a storyboards

---
+++ VkMlApp/VkMlApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VkMlApp
+//
+//  Created by Mike Ulanov on 08.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
